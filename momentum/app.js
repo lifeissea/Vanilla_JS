2ed19f83a0131cac0822de26156aa77f43f0c09c
김여명 <kim-yeomyeong@gim-yeomyeong-ui-MacBookPro.local>
2022-12-09 15:37:07 +0900

    teset

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,13 @@
+let a = 5;
+let b = 6;
+let myName = "kim";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
 console.log("버그발견");
+console.log("Hello " + myName);
+
+myName = "kkim";
+
+console.log("Your new name is " + myName);
