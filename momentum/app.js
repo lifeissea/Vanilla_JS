1a11e01fc59e1a2cfc8d417d128230eb40195ccc
Tomas <lifeissea@naver.com>
2022-12-10 19:24:22 +0900

    test

--- momentum/app.js
+++ momentum/app.js
@@ -1,13 +1,5 @@
-let a = 5;
-let b = 6;
-let myName = "kim";
+function plus(a, b) {
+  console.log(a, b);
+}
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("버그발견");
-console.log("Hello " + myName);
-
-myName = "kkim";
-
-console.log("Your new name is " + myName);
+plus();
